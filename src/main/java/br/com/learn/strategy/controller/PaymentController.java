d41913ec2d7e6275cefb0e6fdb85d2cfa7034610
Pedro Santos <pedro_henriquems321@hotmail.com>
2023-08-06 17:47:55 -0300

    Add ResponseEntity

--- src/main/java/br/com/learn/strategy/controller/PaymentController.java
+++ src/main/java/br/com/learn/strategy/controller/PaymentController.java
@@ -1,5 +1,6 @@
 package br.com.learn.strategy.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,7 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.learn.strategy.dto.PaymentDTO;
 import br.com.learn.strategy.service.PaymentService;
-import br.com.learn.strategy.service.impl.PaymentServiceImpl;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -18,8 +18,11 @@ public class PaymentController {
     private final PaymentService paymentService;
 
     @PostMapping
-    public void processPayment(@RequestBody PaymentDTO paymentDTO) {
+    public ResponseEntity<Void> processPayment(@RequestBody PaymentDTO paymentDTO) {
+       
         paymentService.processPayment(paymentDTO);
+        
+        return ResponseEntity.noContent().build();
     }
     
 }
